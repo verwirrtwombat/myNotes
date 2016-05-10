b62558c8e57787279be07b34d1eff597d0a984e2
verwirrtwombat <chris.seemann@gmail.com>
2016-05-10 08:25:53 -0400

    fixed navbar

--- client/main.js
+++ client/main.js
@@ -1,2 +1,14 @@
 Meteor.subscribe("notes");
 Meteor.subscribe("users");
+
+Notes.allow({
+  insert() { return false; },
+  update() { return false; },
+  remove() { return false; }
+});
+
+Notes.deny({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; }
+});
